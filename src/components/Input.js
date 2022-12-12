bb5b82ce9d2760dfa1cc26ac9e0cfe14c4194663
bopboy <39391891+bopboy@users.noreply.github.com>
2022-12-12 21:49:10 +0900

    벡터 아이콘 사용 및 스타일링

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, TextInput, View } from 'react-native';
 import PropTypes from 'prop-types';
 import { BLACK, GRAY, PRIMARY } from '../colors';
 import { useState } from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export const KeyboardTypes = {
     DEFAULT: 'deault',
@@ -13,7 +14,12 @@ export const ReturnKeyTypes = {
     NEXT: 'next',
 };
 
-const Input = ({ title, placeholder, value, ...props }) => {
+export const IconNames = {
+    EMAIL: 'email',
+    PASSWORD: 'lock',
+};
+
+const Input = ({ title, placeholder, value, iconName, ...props }) => {
     const [isFocused, setIsFocused] = useState(false);
     return (
         <View style={styles.container}>
@@ -26,23 +32,41 @@ const Input = ({ title, placeholder, value, ...props }) => {
             >
                 {title}
             </Text>
-            <TextInput
-                {...props}
-                value={value}
-                style={[
-                    styles.input,
-                    value && styles.hasValueInput,
-                    isFocused && styles.focusedInput,
-                ]}
-                placeholder={placeholder ?? title}
-                placeholderTextColor={GRAY.DEFAULT}
-                autoCapitalize={'none'}
-                autoCorrect={false}
-                textContentType={'none'}
-                keyboardAppearance={'light'}
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-            />
+            <View>
+                <TextInput
+                    {...props}
+                    value={value}
+                    style={[
+                        styles.input,
+                        value && styles.hasValueInput,
+                        isFocused && styles.focusedInput,
+                    ]}
+                    placeholder={placeholder ?? title}
+                    placeholderTextColor={GRAY.DEFAULT}
+                    autoCapitalize={'none'}
+                    autoCorrect={false}
+                    textContentType={'none'}
+                    keyboardAppearance={'light'}
+                    onFocus={() => setIsFocused(true)}
+                    onBlur={() => setIsFocused(false)}
+                />
+                <View style={styles.icon}>
+                    <MaterialIcons
+                        name={iconName}
+                        size={20}
+                        color={(() => {
+                            switch (true) {
+                                case isFocused:
+                                    return PRIMARY.DEFAULT;
+                                case !!value:
+                                    return BLACK;
+                                default:
+                                    return GRAY.DEFAULT;
+                            }
+                        })()}
+                    />
+                </View>
+            </View>
         </View>
     );
 };
@@ -55,6 +79,7 @@ Input.propTypes = {
     title: PropTypes.string,
     placeholder: PropTypes.string,
     value: PropTypes.string,
+    iconName: PropTypes.oneOf(Object.values(IconNames)),
 };
 
 const styles = StyleSheet.create({
@@ -76,6 +101,7 @@ const styles = StyleSheet.create({
         borderColor: GRAY.DEFAULT,
         paddingHorizontal: 20,
         height: 42,
+        paddingLeft: 30,
     },
     hasValueInput: {
         borderColor: BLACK,
@@ -89,6 +115,12 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderColor: PRIMARY.DEFAULT,
         color: PRIMARY.DEFAULT,
+    },
+    icon: {
+        position: 'absolute',
+        left: 8,
+        height: '100%',
+        justifyContent: 'center',
     },
 });
 
