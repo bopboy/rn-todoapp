df8f4c14c925ce94e58e37493e0c8423bf296f1d
bopboy <39391891+bopboy@users.noreply.github.com>
2022-12-14 14:07:14 +0900

    항목 추가 입력창 추가
    (iOS 는 키보드가 가림)

--- src/components/InputFAB.js
+++ src/components/InputFAB.js
@@ -1,18 +1,63 @@
-import { Pressable, StyleSheet } from 'react-native';
+import {
+    Pressable,
+    StyleSheet,
+    TextInput,
+    useWindowDimensions,
+    View,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { PRIMARY, WHITE } from '../colors';
+import { useRef, useState } from 'react';
 
 const InputFAB = () => {
+    const [text, setText] = useState('');
+    const [isOpened, setIsOpened] = useState(false);
+    const inputRef = useRef(null);
+    const windowWidth = useWindowDimensions().width;
+
+    const open = () => {
+        setIsOpened(true);
+        inputRef.current.focus();
+    };
+    const close = () => {
+        setIsOpened(false);
+        inputRef.current.blur();
+    };
+    const onPressButton = () => {
+        isOpened ? close() : open();
+    };
     return (
-        <Pressable
-            style={({ pressed }) => [
-                styles.container,
-                pressed && { backgroundColor: PRIMARY.DARK },
-            ]}
-        >
-            <MaterialCommunityIcons name="plus" size={24} color={WHITE} />
-        </Pressable>
+        <>
+            <View
+                style={[
+                    styles.container,
+                    isOpened && { width: windowWidth - 20 },
+                ]}
+            >
+                <TextInput
+                    ref={inputRef}
+                    value={text}
+                    onChangeText={setText}
+                    style={styles.input}
+                    autoCapitalize={'none'}
+                    autoCorrect={false}
+                    textContentType={'none'}
+                    keyboardAppearance={'light'}
+                    returnKeyType={'done'}
+                    onBlur={close}
+                />
+            </View>
+            <Pressable
+                onPress={onPressButton}
+                style={({ pressed }) => [
+                    styles.container,
+                    pressed && { backgroundColor: PRIMARY.DARK },
+                ]}
+            >
+                <MaterialCommunityIcons name="plus" size={24} color={WHITE} />
+            </Pressable>
+        </>
     );
 };
 
@@ -31,6 +76,11 @@ const styles = StyleSheet.create({
         backgroundColor: PRIMARY.DEFAULT,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    input: {
+        color: WHITE,
+        paddingLeft: 20,
+        paddingRight: 70,
     },
 });
 
